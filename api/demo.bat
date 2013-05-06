8caf2836f87c376fef964fab607c2914ae1bd347
illyfrancis <illy.francis@outlook.com>
2013-05-06 13:58:46 -0400

    update version numbers

--- api/demo.bat
+++ api/demo.bat
@@ -4,4 +4,4 @@ set DB_PORT=27071
 start D:\Mongo\mongodb-win32-i386-2.0.8\bin\startupdemo.bat %DB_PORT%
 
 rem --- start tomcat on 9091 ---
-mvn tomcat7:run -Dweb-port=9091 -Ddb-port=%DB_PORT% -DwarDirectory=D:\Repository\git\bogo\bogosuh.0.5.1
+mvn tomcat7:run -Dweb-port=9091 -Ddb-port=%DB_PORT% -DwarDirectory=D:\Repository\git\bogo\bogosuh.0.5.2
